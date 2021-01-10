fb791c5489bbd66a7ce45155abce33a8191f0ffb
Mikhael Dantas <mikhael.dcs@gmail.com>
2021-01-10 16:47:42 -0300

    initial setup

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import { Container } from './styles';
+import './styles.css'
+
+const Home: React.FC = () => {
+    return (
+        <div className='container home-container'>
+            Seja muito bem-vindo ao site de avaliação. :p
+        </div>
+    )
+}
+
+export default Home;
