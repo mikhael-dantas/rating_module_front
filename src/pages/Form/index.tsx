d2547d9f07eafd0c45db2983d31dc6f265900ea2
Mikhael Dantas <mikhael.dcs@gmail.com>
2021-02-09 22:17:31 -0300

    applying styles to the form

--- src/pages/Form/index.tsx
+++ src/pages/Form/index.tsx
@@ -34,72 +34,81 @@ const Form: React.FC<any> = ({match}) => {
 
     return (
         <Container>
-            <form onSubmit={(e) => handleSubmit(e)}>
-                Pokemon:
-                <input type="text" name='idOrigin' value={idOrigin} onChange={(e) => {setIdOrigin(e.target.value)}}/>
-                Opinião sobre o pokemon:
-                <input type="text" name='title' value={title} onChange={(e) => {setTitle(e.target.value)}}/>
-                Detalhes da avaliação:
-                <input type="text" name='description' value={description} onChange={(e) => {setDescription(e.target.value)}}/>
-                <div>
-                    Estrelas:
-                    <div className="radio">
-                        <label>
-                            <input
-                            type="radio"
-                            value={1}
-                            checked={stars === 1}
-                            onChange={(e) => {setStars(Number(e.target.value))}}
-                            />
-                            1
-                        </label>
+            <h1>Enviar Avaliação</h1>
+            <form className='poke-form' onSubmit={(e) => handleSubmit(e)}>
+                <label className='name'>
+                    Pokemon:<br/>
+                    <input type="text" name='idOrigin' value={idOrigin} onChange={(e) => {setIdOrigin(e.target.value)}}/>
+                </label>
+                <label className='title'>
+                    Opinião sobre o pokemon:<br/>
+                    <input type="text" name='title' value={title} onChange={(e) => {setTitle(e.target.value)}}/>
+                </label>
+                <label className='description'>
+                    Detalhes da avaliação:<br/>
+                    <textarea rows={4} name='description' value={description} onChange={(e) => {setDescription(e.target.value)}}/>
+                </label>
+                <label className='stars'>
+                    <div>
+                        Estrelas:<br/>
+                        <div className="radio">
+                            <label>
+                                <input
+                                type="radio"
+                                value={1}
+                                checked={stars === 1}
+                                onChange={(e) => {setStars(Number(e.target.value))}}
+                                />
+                                1
+                            </label>
+                        </div>
+                        <div className="radio">
+                            <label>
+                                <input
+                                type="radio"
+                                value={2}
+                                checked={stars === 2}
+                                onChange={(e) => {setStars(Number(e.target.value))}}
+                                />
+                                2
+                            </label>
+                        </div>
+                        <div className="radio">
+                            <label>
+                                <input
+                                type="radio"
+                                value={3}
+                                checked={stars === 3}
+                                onChange={(e) => {setStars(Number(e.target.value))}}
+                                />
+                                3
+                            </label>
+                        </div>
+                        <div className="radio">
+                            <label>
+                                <input
+                                type="radio"
+                                value={4}
+                                checked={stars === 4}
+                                onChange={(e) => {setStars(Number(e.target.value))}}
+                                />
+                                4
+                            </label>
+                        </div>
+                        <div className="radio">
+                            <label>
+                                <input
+                                type="radio"
+                                value={5}
+                                checked={stars === 5}
+                                onChange={(e) => {setStars(Number(e.target.value))}}
+                                />
+                                5
+                            </label>
+                        </div>
                     </div>
-                    <div className="radio">
-                        <label>
-                            <input
-                            type="radio"
-                            value={2}
-                            checked={stars === 2}
-                            onChange={(e) => {setStars(Number(e.target.value))}}
-                            />
-                            2
-                        </label>
-                    </div>
-                    <div className="radio">
-                        <label>
-                            <input
-                            type="radio"
-                            value={3}
-                            checked={stars === 3}
-                            onChange={(e) => {setStars(Number(e.target.value))}}
-                            />
-                            3
-                        </label>
-                    </div>
-                    <div className="radio">
-                        <label>
-                            <input
-                            type="radio"
-                            value={4}
-                            checked={stars === 4}
-                            onChange={(e) => {setStars(Number(e.target.value))}}
-                            />
-                            4
-                        </label>
-                    </div>
-                    <div className="radio">
-                        <label>
-                            <input
-                            type="radio"
-                            value={5}
-                            checked={stars === 5}
-                            onChange={(e) => {setStars(Number(e.target.value))}}
-                            />
-                            5
-                        </label>
-                    </div>
-                </div>
-                <input type="submit" value="Enviar" />
+                </label>
+                <input className='submit-button' type="submit" value="Enviar" />
             </form>
         </Container>
     );
--- src/pages/Form/styles.ts
+++ src/pages/Form/styles.ts
@@ -1,3 +1,18 @@
 import styled from "styled-components";
 
-export const Container = styled.div``
+export const Container = styled.div`
+    .poke-form {
+        margin-top: 2rem;
+        display: flex;
+        flex-direction: column;
+    }
+    label + label {
+        margin-top: .9rem;
+    }
+    .description textarea {
+        width: 20rem;
+    }
+    .submit-button {
+        margin-top: 1.5rem;
+    }
+`
