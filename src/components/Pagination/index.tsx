049779c29955ba67adcefaba7c9bf2ca791056ec
Mikhael Dantas <mikhael.dcs@gmail.com>
2021-02-10 06:08:13 -0300

    adding style conditions

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -12,12 +12,18 @@ const Pagination: React.FC<PaginationProps> = ({pages, callback, currentPage}) =
     const array = Array.from(Array(pages).keys())
     return <Container>
         <div className='pagination'>
-            {
-                array.map(page => {
-                    let truePage = page + 1
-                    return (<h2 key={truePage} onClick={() => callback(truePage)}>{truePage}</h2>)
-                })
-            }
+            {array.map(page => {
+                let truePage = page + 1
+                return (
+                    <h2 
+                        key={truePage} 
+                        onClick={() => callback(truePage)}
+                        style={truePage === currentPage ? {background: 'yellow'} : {}}
+                    >
+                        {truePage}
+                    </h2>
+                )
+            })}
         </div>
     </Container>;
 }
