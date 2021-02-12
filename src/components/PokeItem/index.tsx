a80bfd874562342c2c0d89f22da37ace9921a332
Mikhael Dantas <mikhael.dcs@gmail.com>
2021-02-12 19:41:02 -0300

    main api call setup

--- src/components/PokeItem/index.tsx
+++ src/components/PokeItem/index.tsx
@@ -1,35 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { pokeApi } from '../../services/api';
 import Stars from '../Stars';
 
 import { Container } from './styles';
 
-export interface Rating {
-    id: number;
-    id_origin: string;
-    stars_avarage: number;
+
+interface PokeItemProps {
+    listedPoke: any;
 }
 
-
-interface RatingsItemProps {
-    rating: Rating;
-}
-
-const PokeItem: React.FC<RatingsItemProps> = ({ rating }) => {
-    const [pokemon, setPokemon] = useState({ name: "", sprites: { front_default: "" } });
-
-    useEffect(() => {
-        pokeApi.get('/' + rating.id_origin).then(response => {
-            const pokemonData = response.data
-
-            setPokemon(pokemonData)
-        })
-    }, [rating.id_origin])
-
+const PokeItem: React.FC<PokeItemProps> = ({ listedPoke: pokemon }) => {
     return (
         <Container>
-
             <article className="rating-item">
                 <header>
                     <img src={pokemon.sprites.front_default} alt={pokemon.name} />
@@ -47,7 +29,7 @@ const PokeItem: React.FC<RatingsItemProps> = ({ rating }) => {
                     <p>
                         <strong>Avaliação média do Pokemon:</strong>
                     </p>
-                        <Stars stars={rating.stars_avarage}/>
+                        <Stars stars={Number(pokemon.stars_avarage)}/>
                 </footer>
             </article>
         </Container>
--- src/pages/PokeList/index.tsx
+++ src/pages/PokeList/index.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import {pokeApi} from '../../services/api';
+import {api, pokeApi} from '../../services/api';
 
 import { Container } from './styles'
-import PokeItem, {Rating} from '../../components/PokeItem';
+import PokeItem from '../../components/PokeItem';
 import Pagination from '../../components/Pagination';
 
 const PokeList: React.FC = () => {
 
-    const [ratings, setRatings] = useState<Rating[]>([]);
+    const [list, setList] = useState<any[]>([]);
 
     const [totalPages, setTotalPages] = useState(1)
     const [offset, setOffset] = useState(0)
@@ -15,30 +15,42 @@ const PokeList: React.FC = () => {
     const currentPage = Math.ceil((offset+0.1)/limit)
     const setOffsetCallback = (page: number) => {setOffset(page*limit-limit)}
 
-    async function setDataFromPokeApi(offset: number) {
-        const pokes = await pokeApi.get('/', { params: {offset: offset, limit: 1}})
-        const organizedPokes = pokes.data.results.map((poke: any, index: number) => {
-            return {
-                id: index,
-                id_origin: poke.name,
-                stars_avarage: 4.7
-            }
-        })
-        setRatings(organizedPokes)
-        const pages = Math.floor(pokes.data.count/20) 
-        setTotalPages(pages)
-    }
-
     useEffect(() => {
-        setDataFromPokeApi(offset)
+        async function setDataFromApi(offsetArg: number) {
+            let error = 0
+            let organizedPokeList: any[] = []
+    
+            let apiPokeList = await api.get('/pokemons', { params: {offset: offsetArg, limit: 1}}).catch(() => {
+                console.log('error trying to catch the main api. Getting data from pokeapi instead')
+                error = 1
+                return {data: {}}
+            })
+
+            if (error) {
+                apiPokeList = await pokeApi.get('/', { params: {offset: offsetArg, limit: 1}})
+
+                organizedPokeList = await Promise.all(apiPokeList.data.results.map(async (poke: any) => {
+                    const response = await pokeApi.get('/' + poke.name)
+                    response.data.stars_avarage = 4.7
+                    return response.data
+                }))
+            } else {
+                organizedPokeList = apiPokeList.data.results
+            }
+
+            setList(organizedPokeList)
+            const pages = Math.floor(apiPokeList.data.count/20) 
+            setTotalPages(pages)
+        }
+        setDataFromApi(offset)
     }, [offset]);
 
     return (
         <Container className='container'>
             <h1>Lista de pokemons</h1>
             <main>
-                {ratings.map((rating: Rating) => {
-                    return <PokeItem key={rating.id} rating={rating} />;
+                {list.map((listedPoke: any) => {
+                    return <PokeItem key={listedPoke.id} listedPoke={listedPoke} />;
                 })}
             </main>
             <Pagination pages={totalPages} callback={setOffsetCallback} currentPage={currentPage}/>
