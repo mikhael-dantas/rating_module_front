02f3a9f4c40643a4d1c80b9e3626cdb6ef661bb8
Mikhael Dantas <mikhael.dcs@gmail.com>
2021-02-16 23:43:13 -0300

    updating routes

--- src/components/FloatingMenu/index.tsx
+++ src/components/FloatingMenu/index.tsx
@@ -13,7 +13,7 @@ const FloatingMenu: React.FC<{
             style={{display: visible ? 'initial' : 'none'}}
         >
             <div className='menu'>
-                <Link href='/pokemons'><a className='menu-card'>Lista de Pokemons</a></Link>
+                <Link href='/pokemons/1'><a className='menu-card'>Lista de Pokemons</a></Link>
                 <Link href='/ratings'><a className='menu-card'>Lista de Avaliações</a></Link>
                 <Link href='/form'><a className='menu-card'>Avalie um Pokemon</a></Link>
                 {/* <Link to='/config' className='menu-card'>Configurações</Link> */}
