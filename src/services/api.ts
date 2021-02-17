290612b0e1fce3174cbb0895873dea232b5bb750
Mikhael Dantas <mikhael.dcs@gmail.com>
2021-02-16 23:42:42 -0300

    rebuilding api modules

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,9 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// export const api = axios.create({
-//   baseURL: 'http://localhost:3333',
-// });
+export const api = axios.create({
+    baseURL: 'http://localhost:3333',
+});
 
-// export const pokeApi = axios.create({
-//   baseURL: 'https://pokeapi.co/api/v2/pokemon',
-// });
-export const x = 1
+export const pokeApi = axios.create({
+    baseURL: 'https://pokeapi.co/api/v2/pokemon',
+});
