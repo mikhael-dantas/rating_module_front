0c92b2fdeff4ba265274b3849707a34af2495612
Mikhael Dantas <mikhael.dcs@gmail.com>
2021-02-25 22:05:17 -0300

    test

--- src/pages/api/pokemons/[name].ts
+++ src/pages/api/pokemons/[name].ts
@@ -4,16 +4,15 @@ import Pokemon from '../../../database/models/Pokemon'
 export default async function handler(req: any, res: any) {
     const { name } = req.query
 
-    await dbConnect()
-    
     try {
+        await dbConnect()
         const pokemon = await Pokemon.findOne({name: name})
         if (!pokemon) {
             return res.status(500).json({message: 'pokemon not found'})
         }
-        res.status(200).json({ success: true, results: pokemon })
+        return res.status(200).json({ success: true, results: pokemon })
     } catch (error) {
         console.log(error)
-        res.status(400).json({ success: false })
+        return res.status(400).json({ success: false })
     }
 }
